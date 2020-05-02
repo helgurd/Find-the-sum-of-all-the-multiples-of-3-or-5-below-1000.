d0c30722d161c3b637376d0dc0fd2bb1b6cae58f
Helgurd <rekauta@gmail.com>
2020-05-02 03:57:39 +0100

    solve the problem in python
    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of this multiples are 23.

---
+++ mulitiples.py
@@ -0,0 +1,11 @@
+
+#python
+#need to var as one will do the divisable num and next will add and return the result in range of 1000.
+i= 0
+x=1
+for x in range (1000):
+    if x %3 ==0 or x %5 ==0:
+        i +=x
+    print(i)
+
+
